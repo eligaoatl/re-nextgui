71b71837cab1525fdf455886c3f6cd3b59e8f628
eligaoatl <eligaoatl@gmail.com>
2022-05-15 18:14:08 -0400

    add router

--- pages/jenkins/[id].js
+++ pages/jenkins/[id].js
@@ -1,4 +1,7 @@
+import { useRouter } from "next/router"
+
 export default function JenkinsServerPage() {
+  console.log(useRouter)
   return (
     <div>[id]</div>
   )
--- pages/jenkins/index.js
+++ pages/jenkins/index.js
@@ -1,5 +1,16 @@
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
 export default function ListOfJenkinsPage() {
   return (
-    <div>List of Jenkins Servers</div>
+    <div>List of Jenkins Servers
+    <Link href='/'>
+        <h1 className={styles.card}>
+            Back to home page
+        </h1>
+    </Link>
+    </div>
+
+
   )
 }
