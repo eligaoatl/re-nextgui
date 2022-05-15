4522a6458a6a70c2a2e761373443e69c3728cd35
eligaoatl <eligaoatl@gmail.com>
2022-05-15 18:36:07 -0400

    update id

--- pages/jenkins/[id].js
+++ pages/jenkins/[id].js
@@ -3,6 +3,6 @@ import { useRouter } from "next/router"
 export default function JenkinsServerPage() {
   console.log(useRouter)
   return (
-    <div>[id]</div>
+    <div>Server ID: [id]</div>
   )
 }
