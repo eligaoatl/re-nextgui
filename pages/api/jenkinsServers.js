83864fe6667e89c299e50e2b1ed03a8a06239f05
eligaoatl <eligaoatl@gmail.com>
2022-05-16 22:37:30 -0400

    add api call

--- pages/api/jenkinsServers.js
+++ pages/api/jenkinsServers.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const pkg = require('../../package.json')
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(pkg.scripts)
 }
